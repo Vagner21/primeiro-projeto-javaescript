95cf69bee0cbe6f259b5597b81c63da2d29bc6bc
vagner21 <vagner21tsilva@outlook.com>
2023-09-27 20:23:29 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+const convertButtun = document.querySelector(".convert-button")
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector("input-currency").value
+}
+console. log(".convert-button")
